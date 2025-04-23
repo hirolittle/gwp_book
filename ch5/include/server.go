b3dfde9094a83287779bbbd4c73caccbee7edc3d
hiro <hirolittle@163.com>
2025-04-23 16:08:11 +0800

    [ADD] http include action

---
+++ ch5/include/server.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func main() {
+
+	server := http.Server{
+		Addr: ":8080",
+	}
+
+	http.HandleFunc("/process", process)
+
+	server.ListenAndServe()
+
+}
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("ch5/include/t1.html", "ch5/include/t2.html")
+	t.Execute(w, "hello world")
+}

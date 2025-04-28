e0e9f13cc9ced4b1a79fbcc80d698e11fef9886e
hiro <hirolittle@163.com>
2025-04-28 19:23:58 +0800

    [ADD] parse json file with decoder

---
+++ Chapter_7_Creating_Web_Services/json_parsing_decoder/json.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Post struct {
+	Id       int       `json:"id"`
+	Content  string    `json:"content"`
+	Author   Author    `json:"author"`
+	Comments []Comment `json:"comments"`
+}
+
+type Author struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Comment struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func main() {
+
+	jsonFile, err := os.Open("Chapter_7_Creating_Web_Services/json_parsing_decoder/post.json")
+	if err != nil {
+		fmt.Printf("Error opening JSON file: %v\n", err)
+		return
+	}
+	defer jsonFile.Close()
+
+	decoder := json.NewDecoder(jsonFile)
+	for {
+		var post Post
+		err := decoder.Decode(&post)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Printf("Error decoding JSON: %v\n", err)
+			return
+		}
+		fmt.Printf("%#v\n", post)
+	}
+}
